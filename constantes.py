69328eee45e6b1dc8c8c2406f9fbd7d801a31aa6
nicolas <niko-kiki@hotmail.fr>
2018-11-27 08:51:52 +0100

    new card

--- constantes.py
+++ constantes.py
@@ -1,11 +1,14 @@
 ﻿# Créé par Nicolas, le 24/11/2018 en Python 3.2
 
 # parametres de la fenetre
-HEIGHT = 320
-WIDTH = 200
+HEIGHT = 640
+WIDTH = 480
 CARD_HEIGHT = 140
 CARD_WIDTH = 100
+
+CARPET_COLOR = "green"
 
 
 # images cartes
 CARD_THEN = "carteImp.gif"
+CARD_A = "carteA.gif"
--- gui.py
+++ gui.py
@@ -4,14 +4,14 @@ from constantes import *
 class Card(object):
     def __init__(self, image):
         self.image=tk.PhotoImage(file=image)
-        
+
 
 class ErgoGui(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title("Ergo")
-        self.can = tk.Canvas(self, height=HEIGHT, width=WIDTH, bg='blue')
-        self.cardThen = Card(CARD_THEN)
+        self.can = tk.Canvas(self, height=HEIGHT, width=WIDTH, bg=CARPET_COLOR)
+        self.cardThen = Card(CARD_A)
         self.card=self.can.create_image(CARD_WIDTH//2, CARD_HEIGHT//2, image=self.cardThen.image)
         self.can.pack()
         self.can.bind('<Button1-Motion>', func=self.move)
@@ -22,5 +22,5 @@ class ErgoGui(tk.Tk):
 if __name__ == '__main__':
     ergoGui = ErgoGui()
     ergoGui.mainloop()
-    
+
 
